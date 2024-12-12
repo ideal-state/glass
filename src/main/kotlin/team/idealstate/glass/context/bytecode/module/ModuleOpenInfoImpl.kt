df499719e285cf34b13effadcc3ea794551232b7
ketikai <ketikai@idealstate.team>
2024-12-13 06:55:10 +0800

    feat(src): 新增 module-info 的字节码分析及其相关的伪编译等内容。
    [skip ci]
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

---
+++ src/main/kotlin/team/idealstate/glass/context/bytecode/module/ModuleOpenInfoImpl.kt
@@ -0,0 +1,55 @@
+/*
+ *    Copyright 2024 ideal-state
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package team.idealstate.glass.context.bytecode.module
+
+import java.util.LinkedList
+
+class ModuleOpenInfoImpl(
+    override val packageName: String,
+) : ModuleOpenInfo {
+    private val _toModules: LinkedHashSet<String> = linkedSetOf()
+    override val toModules: List<String>
+        get() = LinkedList(_toModules)
+
+    fun to(module: String) {
+        _toModules.add(module)
+    }
+
+    fun unto(module: String) {
+        _toModules.remove(module)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ModuleOpenInfoImpl
+
+        if (packageName != other.packageName) return false
+        if (_toModules != other._toModules) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = packageName.hashCode()
+        result = 31 * result + _toModules.hashCode()
+        return result
+    }
+
+    override fun toString(): String = "ModuleOpenInfoImpl(packageName='$packageName', toModules=$_toModules)"
+}
---
+++ src/main/kotlin/team/idealstate/glass/context/bytecode/module/ModuleRequireInfoImpl.kt
@@ -0,0 +1,48 @@
+/*
+ *    Copyright 2024 ideal-state
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package team.idealstate.glass.context.bytecode.module
+
+class ModuleRequireInfoImpl(
+    override val transitive: Boolean,
+    override val static: Boolean,
+    override val module: String,
+    override val version: String?,
+) : ModuleRequireInfo {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ModuleRequireInfoImpl
+
+        if (transitive != other.transitive) return false
+        if (static != other.static) return false
+        if (module != other.module) return false
+        if (version != other.version) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = transitive.hashCode()
+        result = 31 * result + static.hashCode()
+        result = 31 * result + module.hashCode()
+        result = 31 * result + (version?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String = "ModuleRequireInfoImpl(transitive=$transitive, static=$static, module='$module', version=$version)"
+}
